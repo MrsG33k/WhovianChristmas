6fe4236e1e41a6a12ffb691efd87cb89db11ebf8
Jennie Whittaker <jenniekwhittaker@gmail.com>
2025-12-23 16:37:34 +0000

    Added initial JS array for the Dr Who picker

---
+++ assets/JS/script.js
@@ -0,0 +1,27 @@
+const christmasSpecials = [
+  {
+    title: "The Christmas Invasion",
+    doctor: "10th Doctor (David Tennant)",
+    year: 2005,
+    blurb: "The Doctor recovers from his regeneration and battles the Sycorax.",
+    quote: "'I'm going to get killed by a christmas tree!'",
+    image: "images/the-christmas-invasion.jpg"
+  },
+  
+  {
+    title: "A Christmas Carol",
+    doctor: "11th Doctor (Matt Smith)",
+    year: 2010,
+    blurb: "A timey-wimey reworking of Dickens with flying sharks.",
+    quote: "'In 900 years of time and space, I've never met anyone who wasn't important'",
+    image: "images/a-christmas-carol.jpg"
+  },
+  {
+    title: "The Runaway Bride",
+    doctor: "10th Doctor (David Tennant)",
+    year: 2006,
+    blurb: "Donna Noble, killer robots, and a very angry wedding.",
+    quote: "'I missed my wedding, lost my job and became a widow on the same day. Sort of.'",
+    image: "images/the-runaway-bride.jpg"
+  }
+];
